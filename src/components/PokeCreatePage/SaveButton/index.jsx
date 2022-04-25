ccdc05408c8b9e7ded20f559b8e15221c0ad35be
ivini95 <viniximenes98@gmail.com>
2022-04-25 15:15:24 -0300

    create firedbase file and implement in buttonSave

--- src/components/PokeCreatePage/SaveButton/index.jsx
+++ src/components/PokeCreatePage/SaveButton/index.jsx
@@ -1,13 +1,28 @@
 import { useNavigate } from 'react-router-dom';
 import './style.css'
+import {db} from '../../../dataBase/firerebase.js'
+import {collection, getDocs, addDoc}  from "firebase/firestore";
 
 
 function SaveButton(){
 
   let navigate = useNavigate()
 
-  function save(){
-    navigate("/battleBot/5")
+  async function save(){
+      await addDoc(collection(db, "pokeChars"), {
+        name: "",
+        caracteristics: 
+          {'strength': 0,
+          'ability': 0,
+          'resistence': 0,
+          'armor': 0,
+          'firePower': 0},
+        life: 0,
+        mana: 0,
+        totalPoints: 12
+      });
+      console.log("PokeCreate");
+    navigate('/battle')
   }
 
   return (
---
+++ src/dataBase/firerebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs}  from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWLQVcl0nofSqMBK1mOl6QdjgA5sCStws",
+  authDomain: "pokerpg-9e052.firebaseapp.com",
+  projectId: "pokerpg-9e052",
+  storageBucket: "pokerpg-9e052.appspot.com",
+  messagingSenderId: "180525603638",
+  appId: "1:180525603638:web:4a94068b6d3ca5b63c7197",
+  measurementId: "G-7VEMK3H02K"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
