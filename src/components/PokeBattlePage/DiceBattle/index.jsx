618d9ba0253d3443f23d8c307ee44da3710fe453
ivini95 <viniximenes98@gmail.com>
2022-06-13 12:11:14 -0300

    fix bug when dice repeats

--- src/components/PokeBattlePage/DiceBattle/index.jsx
+++ src/components/PokeBattlePage/DiceBattle/index.jsx
@@ -1,10 +1,9 @@
-import { getDownloadURL, listAll, ref } from 'firebase/storage';
 import { useState, useContext, useEffect } from 'react';
 import { ApiContextBattle } from '../../../contexts/ApiContextBattle';
-import { storageRef } from '../../../dataBase/firerebase';
 import './style.css';
 
 function DiceBattle(){
+  
 
   var cube = document.getElementById('cube');
 
@@ -15,6 +14,7 @@ function DiceBattle(){
     setDiceRolling(true)
 
     var xyRand = getDiceSide(randomNumber)
+
     
     cube.style.transform = 'rotateX('+xyRand[0]+'deg) rotateY('+xyRand[1]+'deg)';
     
@@ -23,39 +23,44 @@ function DiceBattle(){
     }, 2100);
   }
 
-  const [compareDeg, setCompareDeg] = useState(0)
-  const [compareDice, setCompareDice] = useState(0)
+  const randomNumberInicial = Math.floor(Math.random() * 6) + 1;
+
+  const [compareRandomDeg, setCompareRandomDeg] = useState(randomNumberInicial)
 
 function getDiceSide(randomNumber){
+
+  console.log(compareRandomDeg);
 
   var min = 1;
   var max = 6;
 
   const randomDeg = Math.floor(Math.random() * ((max-min) + min))
-  let multipleDeg = randomDeg * 360
-
-  setCompareDeg(multipleDeg)
-  setCompareDice(randomNumber)
-
-  if (multipleDeg == compareDeg && compareDice == randomNumber) {
-    console.log("dado repetiu");
-    multipleDeg = randomDeg * 720
-  } 
+  if (randomDeg == compareRandomDeg) {
+    setCompareRandomDeg(randomDeg + 1)
+  }else {
+    setCompareRandomDeg(randomDeg)
+  }
   
+  let multipleDeg = compareRandomDeg * 360
+
   const deg = [0,0]
+
 
   switch (randomNumber) {
     case 1:
         deg[0] = 0 + multipleDeg
         deg[1] = 0 + multipleDeg
+      
       break;
       case 2:
         deg[0] = 0 + multipleDeg
         deg[1] = 180 + multipleDeg
+       
       break;
       case 3:
         deg[0] = 180 + multipleDeg
         deg[1] = 90 + multipleDeg
+        
       break;
       case 4:
         deg[0] = 180 + multipleDeg
@@ -109,7 +114,7 @@ function getDiceSide(randomNumber){
   return (
     
     <section className="container">
-      <div id="cube" onClick={generateValue}>
+      <div id="cube" className='swing' onClick={generateValue}>
         <div className="front">
           <span className="dot dot1"></span>
         </div>
